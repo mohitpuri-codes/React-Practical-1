33b6cf0c64c916ad6825056e0cad757019149402
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-04-25 11:32:39 +0530

    feat: add basic info

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React Practical 1</h1>
+      <p>Name: Mohitpuri Goswami</p>
+      <p>Intro: Trainee React Developer</p>
+      <p>Email: mohitpuri.goswami@simformsolutions.com</p>
+      <p>
+        Github ID:
+        <a href="https://github.com/mohitpuri-codes">mohitpuri-codes</a>
+      </p>
+    </>
+  );
+}
+
+export default App;
